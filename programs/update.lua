dda7cf4227bb78f3a77cc0df00e94fe414326a57
Nicholas Blott <blottn@tcd.ie>
2018-12-26 19:59:29 +0000

    complete, added some simple comments

--- programs/update.lua
+++ programs/update.lua
@@ -2,6 +2,7 @@
 
 os.loadAPI("json")
 
+-- helper cause im bad at lua
 local function len(tab) do
 	n = 0
 	for k,v in pairs(tab) do
@@ -10,8 +11,20 @@ local function len(tab) do
 	return n
 end
 
-str = http.get("https://api.github.com/repos/blottn/turtles/contents/programs/").readAll()
-programs = json.decode(str)
+local str = http.get("https://api.github.com/repos/blottn/turtles/contents/programs/").readAll()
+local programs = json.decode(str)
+
+local base = "https://raw.githubusercontent.com/blottn/turtles/master/programs/"
+
+if not fs.exists("./prg") then
+	fs.makeDir("./prg")
+end
+
+
 for i = 1,len(programs) do
-	print(programs[i].name)
+	url = base .. programs[i].name
+	c = http.get(url).readAll()
+	h = fs.open("./prg/" .. programs[i].name, "w")
+	h.write(c)
+	h.close()
 end
